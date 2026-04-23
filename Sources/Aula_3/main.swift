28dff654d758d657f5b4c2d0aff2c8c825c2f2ef
Marco Antonio de Camargo <10418309@mackenzista.com.br>
2026-04-23 20:40:33 +0000

    codigos realizados nas aulas 2 a 4

--- Sources/Aula_3/main.swift
+++ Sources/Aula_3/main.swift
@@ -1,3 +1,40 @@
 import Foundation
 
-print("Aula 3 de Swift!!")
+class Cofre{
+    private var saldo: Double
+
+    init(){
+        self.saldo = 0
+    }
+
+
+    public func depositar(quantia: Double){
+        if (quantia <= 0) {
+            print("Erro ao depositar - Quantia <= 0")
+            return
+        }
+
+        self.saldo += quantia
+    }
+
+    public func sacar(quantia: Double){
+        if(quantia > self.saldo) {
+            print("Erro ao sacar - Quantia > saldo")
+            return
+        }
+
+        self.saldo -= quantia
+    }
+
+    public func getSaldo() -> Double{
+        return self.saldo
+    }
+}
+
+print("Criando um cofre válido:")
+let meuCofre = Cofre()
+print("Saldo inicial: \(meuCofre.getSaldo())")
+meuCofre.depositar(quantia: 100.0)
+print("Saldo apos saque: \(meuCofre.getSaldo())")
+meuCofre.depositar(quantia: -100.0)
+meuCofre.sacar(quantia: 200.0)
--- Sources/Aula_4/main.swift
+++ Sources/Aula_4/main.swift
@@ -1,3 +1,92 @@
 import Foundation
 
-print("Aula 4 de Swift!!")
+class User{
+    let nome: String
+    private let cpf: Int
+
+
+    init(nome: String, cpf: Int){
+        self.nome = nome
+        self.cpf = cpf
+    } 
+
+    public func getCPF() -> Int{
+        return cpf
+    }
+}
+
+
+class Conta: User{
+    private var saldo: Double
+    private var score: ScoreCategory
+    private var email: String
+    private var limite: Double
+    private var tipoConta: tipoConta
+    private var senha: String
+
+    init(nome: String, cpf: Int, email: String, tipoConta: tipoConta, senha: String){
+        self.email = email
+        self.senha = senha
+        self.tipoConta = tipoConta
+        saldo = 0.0
+        score = .baixo
+        limite = 0
+
+        super.init(nome: nome, cpf: cpf)
+    }
+
+    public func getScore() -> ScoreCategory{
+        return score
+    }
+
+    public func getEmail() -> String{
+        return email
+    }
+
+    private func setEmail(email: String){
+        self.email = email
+    }
+
+    public func setEmailSenha(senha: String, novoEmail: String){
+        if(self.senha == senha){
+            setEmail(email: novoEmail)
+        }
+    }
+
+
+    public func setSenha(senhaAntiga: String, senhaNova: String){
+        if senhaAntiga == self.senha{
+            senha = senhaNova
+        }
+    }
+
+
+    public func aumentarLimite(score: ScoreCategory){
+        switch score{
+            case .baixo:
+                limite = 600
+            case .medio:
+                limite = 1200
+            case .alto:
+                limite = 2000
+        }
+    }
+
+}
+
+
+enum ScoreCategory{
+    case baixo
+    case medio
+    case alto
+}
+
+enum tipoConta{
+    case corrente
+    case poupanca
+}
+
+
+
+
+var minhaConta = Conta(nome: "Matt", cpf: 123456789, email: "NelsonAndMurdock@marvelmail.com", tipoConta: .poupanca, senha: "1965")

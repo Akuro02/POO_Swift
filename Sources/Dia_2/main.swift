702a1a900a04a66977af8e9c0286427d0ba06344
Marco Antonio de Camargo <10418309@mackenzista.com.br>
2026-05-01 06:49:54 +0000

    Dia_2

--- Sources/Dia_2/main.swift
+++ Sources/Dia_2/main.swift
@@ -36,3 +36,79 @@ class Aparelho: Manutencao{
     }
     
 }
+
+
+protocol Aula{
+    var nome: String {get}
+    var instrutor: String {get}
+    var categoria: String {get}
+    var descricao: String {get}
+}
+
+class TurmaColetiva: Aula {
+    let nome: String
+    let instrutor: String
+    let categoria: String
+    let descricao: String
+
+    let capacidadeMinima: Int
+    let capacidadeMaxima: Int
+
+    private(set) var inscritos: [String] = []
+
+    init(nome: String, instrutor: String, categoria: String, descricao: String, min: Int, max: Int) {
+        self.nome = nome
+        self.instrutor = instrutor
+        self.categoria = categoria
+        self.descricao = descricao
+        self.capacidadeMinima = min
+        self.capacidadeMaxima = max
+    }
+
+    func inscrever(aluno: String){
+        if inscritos.contains(aluno) {
+            print("Aluno \(aluno) ja inscrito")
+            return
+        }
+
+        if inscritos.count >= capacidadeMaxima {
+            print("Erro ao adicionar - Capacidade maxima ja atingida")
+            return
+        }
+
+        inscritos.append(aluno)
+        print("Sucesso, \(aluno) esta inscrito na \(nome)")
+    }
+
+    func remover(aluno: String) {
+        inscritos.removeAll { $0 == aluno }
+    }
+}
+
+class TreinoPersonal: Aula {
+    let nome: String
+    let instrutor: String
+    let categoria: String
+    let descricao: String
+
+    let aluno: String
+    let horario: String
+
+    init(nome: String, instrutor: String, categoria: String, descricao: String, aluno: String, horario: String) {
+        self.nome = nome
+        self.instrutor = instrutor
+        self.categoria = categoria
+        self.descricao = descricao
+        self.aluno = aluno
+        self.horario = horario
+    }
+}
+
+let col1 = TurmaColetiva(nome: "Aula de luta", instrutor: "Ned Stark", categoria: "Luta", descricao: "Aula de luta", min: 1, max: 5)
+col1.inscrever(aluno: "Jon")
+col1.inscrever(aluno: "Robb")
+col1.inscrever(aluno: "Arya")
+col1.inscrever(aluno: "Sansa")
+col1.inscrever(aluno: "Rickon")
+col1.inscrever(aluno: "Jon")
+col1.inscrever(aluno: "Theon")
